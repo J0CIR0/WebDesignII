fc43ba3df44befec49a15319a04f2b80d5f942ec
Josue <clarosrocajosue@gmail.com>
2026-05-06 01:09:37 +0200

    agregando rutas a los usuarios

--- JociroPy/routes/usuarioRoutes.js
+++ JociroPy/routes/usuarioRoutes.js
@@ -4,9 +4,12 @@ const {
 	loginUsuario,
 	obtenerPerfil
 } = require('../controllers/usuarioController');
+const { registrarUsuario, loginUsuario, obtenerPerfil, obtenerSaldo, recargarCredits } = require('../controllers/usuarioController');
 const verificarToken = require('../middleware/authMiddleware');
 const router = express.Router();
 router.post('/registro', registrarUsuario);
 router.post('/login', loginUsuario);
 router.get('/perfil', verificarToken, obtenerPerfil);
+router.get('/saldo', verificarToken, obtenerSaldo);
+router.post('/recargar', verificarToken, recargarCredits);
 module.exports = router;
