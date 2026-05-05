b06af8e2d4003858b59acd66e5c8bbc63a0c469a
Josue <clarosrocajosue@gmail.com>
2026-05-06 01:52:41 +0200

    agregando pagina de subastas 2

---
+++ JociroPy/src/pages/SubastasPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SubastaLista from '../components/SubastaLista';
+
+const SubastasPage = () => {
+    return (
+        <div className="container">
+            <SubastaLista />
+        </div>
+    );
+};
+
+export default SubastasPage;
