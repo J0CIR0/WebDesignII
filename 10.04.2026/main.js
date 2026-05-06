77158ce7b8901c0f167ca17754731872a80c55e6
Josue <clarosrocajosue@gmail.com>
2026-05-06 15:04:16 +0200

    migrando los archivos

--- 10.04.2026/main.js
+++
@@ -1,8 +0,0 @@
-const parrafo = document.getElementById('miParrafo');
-const boton = document.getElementById('miBoton');
-let iteracion = 0;
-boton.addEventListener('click', function() {
-    iteracion += 1;
-    parrafo.textContent = `iteracion ${iteracion}`;
-}
-);
